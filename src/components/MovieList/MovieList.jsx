dab2f92579a618e1992e7bd97987c474ed51cd18
tobias <tobias.harasim@seznam.cz>
2025-05-13 17:06:59 +0200

    sol

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,12 @@
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.length > 0 ? (
+      movies.map((movie, index) => <MovieCard key={index} movie={movie} />)
+    ) : (
+      <p>No movies available</p>
+    )}
+  </div>
+);
