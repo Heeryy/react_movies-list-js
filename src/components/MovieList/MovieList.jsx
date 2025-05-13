e3686ba14e45c264b086c94b2d767495cd0c9812
tobias <tobias.harasim@seznam.cz>
2025-05-13 17:13:05 +0200

    solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.length > 0 ? (
-      movies.map((movie, index) => <MovieCard key={index} movie={movie} />)
+      movies.map(movie => <MovieCard key={movie.imdbId} movie={movie} />)
     ) : (
       <p>No movies available</p>
     )}
